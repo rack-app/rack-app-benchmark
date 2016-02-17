bda41e665e02f45b379cf46690cd71c32dc25e4e
Adam Luzsi <adamluzsi@gmail.com>
2016-02-17 22:07:32 +0100

    feat: benchmark spec

--- apps/rack_app.rb
+++ apps/rack_app.rb
@@ -5,12 +5,15 @@ class RackApp < Rack::App
     'hello'
   end
 
-  CUSTOM_ENDPOINT_NUMBER.times do |index|
-
+  1000000.times do |index|
     get "/#{index}" do
       'hello'
     end
-
   end
+
+  1000000.times do |index|
+    define_method("endpoint_helper_method_#{index}"){}
+  end
+
 
 end
--- apps/rack_skeleton.rb
+++ apps/rack_skeleton.rb
@@ -1,11 +1,26 @@
 class RackSkeleton
 
   def call(env)
-    ['200', {'Content-Type' => 'text/html'}, ['A barebones rack app.']]
+
+    request_path = env[Rack::REQUEST_PATH]
+
+    case true
+
+      when request_path == '/'
+        ['200', {'Content-Type' => 'text/html'}, ['static endpoint']]
+
+      when request_path =~ /^\/users\/\d+$/
+        ['200', {'Content-Type' => 'text/html'}, ['dynamic endpoint']]
+
+      else
+        ['404', {}, ['404 Not Found']]
+
+    end
+
   end
 
-  CUSTOM_ENDPOINT_NUMBER.times do |index|
-    define_method("endpoint_#{index}"){}
+  1000000.times do |index|
+    define_method("endpoint_helper_method_#{index}"){}
   end
 
 end
